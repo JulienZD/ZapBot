b6dbd3f0efa98f19f0fa582ee9c069713fb66ff9
Julien <jzapata@live.nl>
2020-06-20 17:25:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ZapBot
+A Discord bot that I'm working on in my spare time
+
+The main feature I'm hoping to implement is a reminder system.
---
+++ index.js
@@ -0,0 +1,27 @@
+const { prefix, status } = require("./config.json");
+const token = process.env.DISCORD_BOT_TOKEN;
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  
+  client.user.setActivity(status, { type: "LISTENING"})
+  .then(presence => console.log(`Status set to "${presence.activities[0].name}"`))
+  .catch(console.error);
+});
+ 
+client.on('message', msg => {
+  if (msg.content === prefix + 'ping') {
+    msg.channel.send('pongf');
+
+  }
+  if (msg.content === 'rosh') {
+    setTimeout(function() {
+      msg.channel.send('is great')
+    }, 5000);
+  }
+});
+ 
+
+client.login(token);
