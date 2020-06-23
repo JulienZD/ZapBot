1a045744e9b60cf597087d3760bc85367e4284fe
Julien <jzapata@live.nl>
2020-06-23 11:46:47 +0200

    Remove deprecated tests

--- test/test.js
+++ test/test.js
@@ -1,31 +1,8 @@
 const assert = require('assert');
-const calculate = require('../commands/calculate');
-describe('Calculate', function() {
-	describe('#insufficientArgs(args)', function() {
-		it('should return true when the amount of arguments is less than 3', function() {
-			assert.equal(calculate.insufficientArgs(['1', '2']), true);
-		});
-		it('should return false when the amount of arguments is 3 or more', function() {
-			assert.equal(calculate.insufficientArgs(['*', '2', '3']), false);
-			assert.equal(calculate.insufficientArgs(['*', '2', '3', '4']), false);
-		});
-	});
-	describe('#isOperator(firstArg)', function() {
-		it('should return false when the first argument is not one of *, +, / or -', function() {
-			assert.equal(calculate.isOperator(' '), false);
-			assert.equal(calculate.isOperator('w'), false);
-		});
-		it('should return true when the first argument is *', function() {
-			assert.equal(calculate.isOperator('*'), true);
-		});
-		it('should return true when the first argument is /', function() {
-			assert.equal(calculate.isOperator('/'), true);
-		});
-		it('should return true when the first argument is -', function() {
-			assert.equal(calculate.isOperator('+'), true);
-		});
-		it('should return true when the first argument is +', function() {
-			assert.equal(calculate.isOperator('-'), true);
+describe('<name>', function() {
+	describe('<method>', function() {
+		it('should return <result> when <requirement>', function() {
+			assert.equal(1, true);
 		});
 	});
 });
