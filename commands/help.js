12ed230a272f45e7481b39d68a8aa3850fa29e9b
Julien <jzapata@live.nl>
2020-06-26 22:14:38 +0200

    Move all commands to embed

--- commands/help.js
+++ commands/help.js
@@ -43,13 +43,15 @@ function sendCommandHelp(message, command) {
 }
 
 function sendAllCommandsHelp(message) {
-	const data = [];
 	const { commands } = message.client;
-	data.push('Here\'s a list of all commands:');
-	data.push(commands.map(command => command.name).join('\n'));
-	data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+	const embed = new Discord.MessageEmbed()
+		.setTitle('Commands')
+		.setThumbnail('https://i.imgur.com/beKuLLM.png');
 
-	return message.author.send(data, { split: true })
+	embed.setDescription(commands.map(command => command.name).join('\n'));
+	embed.setFooter(`You can send \`${prefix}help [command name]\` to get info on a specific command!`);
+
+	return message.author.send(embed)
 		.then(() => {
 			if (message.channel.type === 'dm') return;
 			message.reply('I\'ve sent you a DM with a list of commands.');
