a149f0c1d1518416a2d43acca2da9400e8561a1c
Julien <jzapata@live.nl>
2020-06-26 17:55:58 +0200

    Add reload command

---
+++ commands/reload.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+module.exports = {
+	name: 'reload',
+	description: 'Reloads all commands',
+	creatorOnly: true,
+	execute(message, args) {
+		const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+		for (const file of commandFiles) {
+			delete require.cache[require.resolve(`./${file}`)];
+			try {
+				const newCommand = require(`./${file}`);
+				message.client.commands.set(newCommand.name, newCommand);
+			}
+			catch (error) {
+				console.log(error);
+				return message.channel.send('There was an error reloading the commands');
+			}
+		}
+		console.log('Reloaded all commands');
+		message.channel.send('Reloaded all commands.');
+	},
+};
