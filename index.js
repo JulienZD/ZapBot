3d62c1da326f360676f015a3e65df1f950ae2c59
Julien <jzapata@live.nl>
2020-06-26 18:27:29 +0200

    Improve log timestamp format

--- index.js
+++ index.js
@@ -83,7 +83,10 @@ client.login(token);
 function initConsoleLogTimeStamps() {
 	console.logCopy = console.log.bind(console);
 	console.log = function(data) {
-		const currentDate = '[' + new Date().toUTCString() + ']:';
-		this.logCopy(currentDate, data);
+		const now = new Date();
+		const currentDate = now.toISOString().substr(0, 10);
+		const currentTime = now.toISOString().substr(11, 8);
+		const currentDateTime = `[${currentDate} ${currentTime}]:`;
+		this.logCopy(currentDateTime, data);
 	};
 }
