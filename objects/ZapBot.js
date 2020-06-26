2aaf378fb7bf2bcb25a1e7852a6506cdd4338226
Julien <jzapata@live.nl>
2020-06-27 00:36:52 +0200

    Create embed with default values

---
+++ objects/ZapBot.js
@@ -0,0 +1,31 @@
+const MessageEmbed = require('discord.js').MessageEmbed;
+const { creatorId, iconURL } = require('../config.json');
+
+class ZapMessageEmbed extends MessageEmbed {
+	static creditField = {
+		name: '\u200b',
+		value: `_ZapBot created by @${creatorId}_`,
+	};
+	constructor() {
+		super();
+		super.setThumbnail(iconURL)
+			.addField(ZapMessageEmbed.creditField.name, ZapMessageEmbed.creditField.value);
+	}
+	addField(name, value, inline) {
+		return this.addFields( { name: name, value: value, inline: inline} );
+	}
+	addFields(...embedFields) {
+		// Prevent credit field from being deleted.
+		if (this.fields.length === 25) return console.log('ZapBot.addFields: Field limit reached, not adding any more fields.');
+	
+		super.addFields(...embedFields);
+		this.moveCreditFieldToEnd();
+		return this;
+	}
+	moveCreditFieldToEnd() {
+		const data = this.fields;
+		data.push(data.splice(data.findIndex(f => f.value === ZapMessageEmbed.creditField.value), 1)[0]);
+	}
+}
+
+module.exports.ZapMessageEmbed = ZapMessageEmbed;
