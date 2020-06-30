58f5d59211cb0466f9c9b860c1c59db9a419a339
Julien <jzapata@live.nl>
2020-06-30 17:06:56 +0200

    cleanup: extract functions

---
+++ ZapBot/message-handler.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js');
+const { creatorId: CREATOR_ID, prefix: PREFIX, defaultCooldown: DEFAULT_COOLDOWN } = require('../config.json');
+const COOLDOWNS = new Discord.Collection();
+
+function handle(client) {
+	client.on('message', message => {
+		if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+		let args = message.content.slice(PREFIX.length).split(/ +/);
+		let commandName = args.shift().toLowerCase();
+
+		let command = client.commands.get(commandName)
+			|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+		if (!command) return;
+
+		if (command.creatorOnly && message.author.id !== CREATOR_ID) return message.reply('This command is not available to you.');
+
+		if (command.args && !args.length) {
+			let reply = `No arguments provided for \`${command.name}\` command.`;
+
+			if (command.usage) {
+				reply += `\nUsage: \`${PREFIX}${command.name} ${command.usage}\``;
+			}
+
+			return message.channel.send(reply);
+		}
+
+		if (!COOLDOWNS.has(command.name)) {
+			COOLDOWNS.set(command.name, new Discord.Collection());
+		}
+
+		let now = Date.now();
+		let timestamps = COOLDOWNS.get(command.name);
+		let cooldownAmount = (command.cooldown || DEFAULT_COOLDOWN) * 1000;
+
+		if (timestamps.has(message.author.id)) {
+			let expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+			if (now < expirationTime) {
+				let timeLeft = (expirationTime - now) / 1000;
+				return message.reply(`Please wait another ${timeLeft.toFixed(1)} second(s) before reusing the \`${command.name}\` command.`);
+			}
+		}
+		timestamps.set(message.author.id, now);
+		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+		try {
+			command.execute(message, args);
+		}
+		catch (error) {
+			console.error(error);
+			message.reply('there was an error trying to execute that command!');
+		}
+	});
+}
+
+module.exports.handle = handle;
---
+++ ZapBot/setup.js
@@ -0,0 +1,34 @@
+const { creatorId: CREATOR_ID } = require('../config.json');
+const { handle: handleMessages } = require('./message-handler');
+const Discord = require('discord.js');
+
+let client;
+
+function init(theClient) {
+	client = theClient;
+	handleErrors();
+	loadCommands();
+	handleMessages(client);
+}
+
+function handleErrors() {
+	process.on('unhandledRejection', error => console.error('Unhandled promise rejection:', error));
+	client.on('shardError', error => console.error('A websocket connection encountered an error:', error));
+}
+
+function loadCommands() {
+	let commandLoader = require('../libs/load-commands');
+	client.commands = new Discord.Collection();
+	commandLoader.load('./commands', client.commands);
+}
+
+async function initZapBot() {
+	const ZapBot = require('../objects/ZapBot');
+	let mgr = new Discord.UserManager(client);
+	let user = await mgr.fetch(CREATOR_ID);
+	ZapBot.ZapMessageEmbed.creditField.value = `_ZapBot created by ${user.toString()}_`;
+	console.log('ZapBot initialized');
+}
+
+module.exports.init = init;
+module.exports.initZapBot = initZapBot;
