ead83ff8a804ef10d8d88f4a21ebaec8fa484eea
Julien <jzapata@live.nl>
2020-06-30 21:54:29 +0200

    Add timestamps to console.debug()

--- libs/log-timestamps.js
+++ libs/log-timestamps.js
@@ -1,12 +1,19 @@
 function init() {
 	console.logCopy = console.log.bind(console);
-	console.log = function(data) {
-		let now = new Date();
-		let currentDate = now.toISOString().substr(0, 10);
-		let currentTime = now.toISOString().substr(11, 8);
-		let currentDateTime = `[${currentDate} ${currentTime}]:`;
-		this.logCopy(currentDateTime, data);
+	console.log = function(...data) {
+		this.logCopy(`${getDateTimestamp()} ${data.join(' ')}`);
 	};
+	console.debugCopy = console.debug.bind(console);
+	console.debug = function(...data) {
+		this.debugCopy(`${getDateTimestamp()} ${data.join(' ')}`);
+	};
+}
+
+function getDateTimestamp() {
+	let now = new Date();
+	let currentDate = now.toISOString().substr(0, 10);
+	let currentTime = now.toISOString().substr(11, 8);
+	return `[${currentDate} ${currentTime}]:`;
 }
 
 module.exports.init = init;
