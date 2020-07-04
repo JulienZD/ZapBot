c2abc74f5e31276df7590c52e64725c2dfe055ef
Julien <jzapata@live.nl>
2020-07-04 16:40:30 +0200

    Add counting linked to db

--- commands/test-count.js
+++ commands/test-count.js
@@ -1,10 +1,26 @@
+const { Count } = require('../ZapBot/sql');
+
 module.exports = {
 	name: 'test count',
 	description: 'Adds 1 to a counter linked to your account in the database',
 	aliases: ['tc'],
 	cooldown: 1,
-	execute(message) {
-		message.channel.send('Done!')
-			.then(msg => msg.react('✅'));
+	async execute(message) {
+		let countEntry = await Count.findOne({ where: { userId: message.author.id } });
+		if (!countEntry) {
+			try {
+				countEntry = await Count.create({
+					userId: message.author.id,
+				});
+			}
+			catch (err) {
+				console.log(err);
+				return message.reply('Something went horribly wrong.');
+			}
+		}
+		countEntry.amount = countEntry.amount + 1;
+		await countEntry.save();
+	
+		message.react('✅');
 	},
 };
--- commands/test-get-count.js
+++ commands/test-get-count.js
@@ -1,10 +1,16 @@
+const { Count } = require('../ZapBot/sql');
+
 module.exports = {
 	name: 'test get count',
 	description: 'Displays the value of the counter linked to your account in the database',
 	aliases: ['gc'],
 	cooldown: 1,
-	execute(message) {
-		let amount = 0;
-		message.channel.send(`Your counter is at ${amount}!`);
+	async execute(message) {
+		let countEntry = await Count.findOne({ where: { userId: message.author.id } });
+		if (!countEntry) {
+			return message.reply('You don\'t have a count!');
+		}
+		
+		message.channel.send(`Your count is at ${countEntry.amount}`);
 	},
 };
