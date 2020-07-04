02aeb0e5227029485055dbb77c17292d546c1b1b
Julien <jzapata@live.nl>
2020-07-04 15:04:44 +0200

    Catch error

--- commands/prune.js
+++ commands/prune.js
@@ -10,10 +10,13 @@ module.exports = {
 		const amount = parseInt(args[0]) + 1; // Avoid pruning the sent message
 		if (isNaN(amount)) return message.reply('please enter a number.');
 
-		if (amount < 2 || amount > 100) {
+		if (amount <= 1 || amount > 100) {
 			return message.reply('add a loop here');
 		}
 		
-		message.channel.bulkDelete(amount);
+		message.channel.bulkDelete(amount, true).catch(err => {
+			console.error(err);
+			message.channel.send('there was an error trying to prune messages in this channel.');
+		});
 	},
 };
