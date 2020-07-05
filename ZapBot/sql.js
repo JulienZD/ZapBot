41edb8ad9d77c7c5bfc1e43813ade155f73cbe8c
Julien <jzapata@live.nl>
2020-07-05 21:31:00 +0200

    Track command usage per user

--- ZapBot/sql.js
+++ ZapBot/sql.js
@@ -35,8 +35,27 @@ const UserCmdCount = sequelize.define('user-command-counter', {
 	},
 });
 
+function countCommand(command, user) {
+	let userCommandUsageEntry = await UserCmdCount.findOne({ where: { userId: user.id, commandName: command.name } });
+		if (!userCommandUsageEntry) {
+			try {
+				userCommandUsageEntry = await UserCmdCount.create({
+					userId: message.author.id,
+					commandName: command.name,
+				});
+			}
+			catch (err) {
+				console.log(err);
+				return message.reply('Something went horribly wrong.');
+			}
+		}
+		userCommandUsageEntry.amount = userCommandUsageEntry.amount + 1;
+		await countEntry.save();
+}
+
 module.exports = {
 	Count: Count,
 	UserCmdCount: UserCmdCount,
 	sync: () => sequelize.sync(),
+	countCommand: countCommand,
 }
