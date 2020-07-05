c64bb7d118b1df1ad8f84ce412736fab5ab5a0be
Julien <jzapata@live.nl>
2020-07-05 22:52:13 +0200

    Add ability to request a user's command usage

--- commands/usage.js
+++ commands/usage.js
@@ -4,19 +4,38 @@ module.exports = {
 	name: 'usage',
 	description: 'Displays the amount of times a command has been used.',
 	cooldown: 5,
-	async execute(message, args) {
-		let userCommandUsageEntries = await UserCmdCount.findAll({
-			attributes: ['amount'],
-			where: { userId: message.author.id } 
-		});
-		if (!userCommandUsageEntries) {
-			return message.reply('You don\'t have a count!');
+	async execute(message) {
+		if (!message.mentions.users.size) {
+			let userCommandUsageEntries = await getUserEntries(message.author.id);
+			if (!userCommandUsageEntries) {
+				return message.reply('You don\'t have a count!');
+			}
+			let amount = getTotalAmount(userCommandUsageEntries);
+			return message.reply(`you have used a total of ${amount} commands.`);
 		}
-		let totalAmount = -1; // account for this command call
-		for (const entry of userCommandUsageEntries) {
-			totalAmount += entry.amount;
+		else {
+			let requestedUser = message.mentions.users.first();
+			let commandUsageEntries = await getUserEntries(requestedUser.id);
+			
+			let amount = getTotalAmount(commandUsageEntries);
+			if (!amount) return message.reply('I don\'t have any info on that user.');
+			
+			return message.channel.send(`${requestedUser} has used ${amount} commands.`);
 		}
-
-		message.reply(`you have used a total of ${totalAmount} commands.`);
 	},
 };
+
+function getTotalAmount(entries) {
+	let total = 0;
+	for (const entry of entries) {
+		total += entry.amount;
+	}
+	return total;
+}
+
+async function getUserEntries(id) {
+	return await UserCmdCount.findAll({
+		attributes: ['amount'],
+		where: { userId: id } 
+	});
+}
