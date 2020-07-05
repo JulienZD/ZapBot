005dc3d3284621db1012d25da3ca8837923ea6ed
Julien <jzapata@live.nl>
2020-07-05 23:34:34 +0200

    Add option to display usage per command

--- commands/usage.js
+++ commands/usage.js
@@ -6,25 +6,60 @@ module.exports = {
 	cooldown: 5,
 	usage: '[@user] [command]',
 	async execute(message, args) {
+		let command;
+
 		if (!message.mentions.users.size) {
-			let userCommandUsageEntries = await getUserEntries(message.author.id);
-			if (!userCommandUsageEntries) {
-				return message.reply('You don\'t have a count!');
-			}
-			let amount = getTotalAmount(userCommandUsageEntries);
-			return message.reply(`you have used a total of ${amount} commands.`);
+			if (!args.length) return sendTotalUserCommandUsages(message);
+
+			command = getCommand(message.client, args[0]);
+			if (!command) return message.reply('that command doesn\'t exist!');
+			
+			let entries = await getUserEntries(message.author.id, command.name);
+
+			let amount = getTotalAmount(entries);
+
+			return message.reply(`you have used \`${command.name}\` ${amount} times.`);
 		}
 		else {
 			let requestedUser = message.mentions.users.first();
-			let commandUsageEntries = await getUserEntries(requestedUser.id);
 			
-			let amount = getTotalAmount(commandUsageEntries);
-			if (!amount) return message.reply('I don\'t have any info on that user.');
+			if (args.length == 1) return sendTotalCommandUsagesFor(message, requestedUser);
 
-			return message.channel.send(`${requestedUser} has used ${amount} commands.`);
+			// Needs more validation. '!usage help @user' results in command doesn't exist
+			command = getCommand(message.client, args[1]);
+			if (!command) return message.reply('that command doesn\'t exist!');
+
+			let entries = await getUserEntries(requestedUser.id, command.name);
+
+			let amount = getTotalAmount(entries);
+
+			return message.channel.send(`${requestedUser} has used \`${command.name}\` ${amount} times.`)
 		}
 	},
 };
+
+function getCommand(client, name) {
+	return client.commands.get(name) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(name));
+}
+
+async function sendTotalCommandUsagesFor(message, user) {
+	let commandUsageEntries = await getUserEntries(user.id);
+			
+	let amount = getTotalAmount(commandUsageEntries);
+	if (!amount) return message.reply('I don\'t have any info on that user.');
+
+	return message.channel.send(`${user} has used ${amount} commands.`);
+}
+
+async function sendTotalUserCommandUsages(message) {
+	let userCommandUsageEntries = await getUserEntries(message.author.id);
+	if (!userCommandUsageEntries) {
+		return message.reply('You don\'t have a count!');
+	}
+	let amount = getTotalAmount(userCommandUsageEntries);
+
+	return message.reply(`you have used a total of ${amount} commands.`);
+}
 
 function getTotalAmount(entries) {
 	let total = 0;
