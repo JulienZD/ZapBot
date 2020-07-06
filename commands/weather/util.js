864903ac918def2185838962b2905f48b6aa2ac7
Julien <jzapata@live.nl>
2020-07-06 15:16:49 +0200

    Retrieve the city object for a user's fav city

--- commands/weather/util.js
+++ commands/weather/util.js
@@ -4,8 +4,13 @@ const { WeatherFavourite } = require('../../ZapBot/sql');
 const findCity = (cityToFind) => cities.find(city => cityToFind.toUpperCase() === city.name.toUpperCase());
 
 async function getFavCity(userId) {
-	let entry = await WeatherFavourite.findOne({ where: { userId: userId } });
-	return entry.favouriteCity; //{ name: 'The Hague' }
+	let entry = await WeatherFavourite.findOne({
+		where: { 
+			userId: userId 
+		} 
+	});
+
+	return findCity(entry.favouriteCity);
 }
 
 module.exports.getFavCity = getFavCity;
