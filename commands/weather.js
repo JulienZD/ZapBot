42e092fcd3eaa9b2c8a10bb471f8a70570bd781f
Julien <jzapata@live.nl>
2020-07-06 15:23:03 +0200

    Validate city object

--- commands/weather.js
+++ commands/weather.js
@@ -1,12 +1,23 @@
+const { WeatherFavourite } = require('../ZapBot/sql');
+const { findCity, getFavCity } = require('./weather/util');
+const Discord = require('discord.js');
+
 module.exports = {
 	name: 'weather',
 	description: 'Displays the weather in any city',
 	aliases: ['w'],
 	cooldown: 5,
-	execute(message) {
-		let temp = 15.76;
-		let city = 'The Hague';
+	async execute(message, args) {
+		let city = await getFavCity(message.author.id);;
+
+		if (args.length) {
+			let cityToFind = args.join(' ');
+			city = findCity(cityToFind);
+		}
 		
-		message.channel.send(`The temperature in ${city} is ${temp}.`);
+		if (!city) {
+			// Invalid city or user doesn't have a favourite city set
+			return message.reply(`please provide a valid city or set your favourite city by using \`${message.client.config.prefix}setfavcity\``);
+		}
 	},
 };
