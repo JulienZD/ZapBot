294800673b303bcf764e2ebcf1d84b45901ba7c3
Julien <jzapata@live.nl>
2020-07-06 16:55:35 +0200

    Add basic info to weather embed

--- commands/weather.js
+++ commands/weather.js
@@ -1,5 +1,5 @@
 const { WeatherFavourite } = require('../ZapBot/sql');
-const { findCity, getFavCity } = require('./weather/util');
+const { findCity, getFavCity, getWeatherForecast } = require('./weather/util');
 const Discord = require('discord.js');
 
 module.exports = {
@@ -19,10 +19,19 @@ module.exports = {
 			// Invalid city or user doesn't have a favourite city set
 			return message.reply(`please provide a valid city or set your favourite city by using \`${message.client.config.prefix}setfavcity\``);
 		}
+
 		let embed = new Discord.MessageEmbed()
 			.setTitle(`${city.name} weather:`);
 
-		embed.addField('\u200b', '_Weather information provided by [OpenWeather](https://www.openweather.com/)._');
+		await getWeatherForecast(city)
+			.then(weatherData => {
+				embed.setThumbnail(`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`);
+				embed.addField('Weather', weatherData.weather[0].main);
+				embed.addField('Temperature', `${weatherData.main.temp}°C\nFeels like: ${weatherData.main.feels_like}°C`);
+				embed.setTimestamp();
+			});
+
+		embed.addField('\u200b', '_Weather information provided by [OpenWeather](https://www.openweathermap.org/)._');
 		message.channel.send(embed);
 	},
 };
