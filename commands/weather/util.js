355edea14e027fefb4bd89b7dd63def84cb385bc
Julien <jzapata@live.nl>
2020-07-06 14:55:58 +0200

    Add function to get a user's fav city

--- commands/weather/util.js
+++ commands/weather/util.js
@@ -1,3 +1,10 @@
 const cities = require('./city.list.min.json');
+const { WeatherFavourite } = require('../../ZapBot/sql');
 
+async function getFavCity(userId) {
+	let entry = await WeatherFavourite.findOne({ where: { userId: userId } });
+	return entry.favouriteCity; //{ name: 'The Hague' }
+}
+
+module.exports.getFavCity = getFavCity;
 module.exports.findCity = (cityToFind) => cities.find(city => cityToFind.toUpperCase() === city.name.toUpperCase());
