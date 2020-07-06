3a261f146273a581ea55c729336550214c681da3
Julien <jzapata@live.nl>
2020-07-06 16:54:38 +0200

    Request forecast from OpenWeatherMap

--- commands/weather/util.js
+++ commands/weather/util.js
@@ -1,5 +1,6 @@
 const cities = require('./city.list.min.json');
 const { WeatherFavourite } = require('../../ZapBot/sql');
+const axios = require('axios');
 
 const findCity = (cityToFind) => cities.find(city => cityToFind.toUpperCase() === city.name.toUpperCase());
 
@@ -13,5 +14,17 @@ async function getFavCity(userId) {
 	return findCity(entry.favouriteCity);
 }
 
+function getWeatherForecast(city) {
+	const url = `https://api.openweathermap.org/data/2.5/weather?id=${city.id}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`;
+	return new Promise((resolve, reject) => {
+		resolve(axios.get(url)
+			.then(res => {
+			return res.data;
+			})
+			.catch(err => console.log(err)));
+	});
+}
+
 module.exports.getFavCity = getFavCity;
 module.exports.findCity = findCity;
+module.exports.getWeatherForecast = getWeatherForecast;
