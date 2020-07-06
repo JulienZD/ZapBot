ca32fd594c0452fd40d5964b4d143b7566ee7403
Julien <jzapata@live.nl>
2020-07-06 15:16:28 +0200

    Move function definition

--- commands/weather/util.js
+++ commands/weather/util.js
@@ -1,10 +1,12 @@
 const cities = require('./city.list.min.json');
 const { WeatherFavourite } = require('../../ZapBot/sql');
 
+const findCity = (cityToFind) => cities.find(city => cityToFind.toUpperCase() === city.name.toUpperCase());
+
 async function getFavCity(userId) {
 	let entry = await WeatherFavourite.findOne({ where: { userId: userId } });
 	return entry.favouriteCity; //{ name: 'The Hague' }
 }
 
 module.exports.getFavCity = getFavCity;
-module.exports.findCity = (cityToFind) => cities.find(city => cityToFind.toUpperCase() === city.name.toUpperCase());
+module.exports.findCity = findCity;
