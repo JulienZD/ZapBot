2a8a8504f42bfa83c01ca5daac629f4f3e593ff8
Julien <jzapata@live.nl>
2020-07-08 22:33:33 +0200

    WIP - create table definitions

--- ZapBot/sql.js
+++ ZapBot/sql.js
@@ -33,16 +33,83 @@ const UserCmdCount = sequelize.define('user-command-counter', {
 	},
 });
 
+// const User = sequelize.define('user', {
+// 	id: {
+// 		type: Sequelize.STRING,
+// 		unique: true,
+// 	}
+// });
+
+// const City = sequelize.define('city', {
+// 	name: {
+// 		type: Sequelize.STRING,
+// 		unique: true,
+// 	}
+// });
+// User.hasOne(City); // NULL allowed
+
+// const Command = sequelize.define('command', {
+// 	name: Sequelize.STRING,
+// });
+
+
+// const BotDMChannel = sequelize.define('botDMChannel', {
+// 	id: {
+// 		type: Sequelize.STRING,
+// 		unique: true,
+// 	}	
+// });
+// User.hasOne(BotDMChannel) // NULL allowed
+
+// const DMCommand = sequelize.define('DM_command', {
+// 	usages: {
+// 		type: Sequelize.INTEGER,
+// 		defaultValue: 0,
+// 	}
+// });
+// DMCommand.hasOne(BotDMChannel);
+// DMCommand.hasOne(Command);
+
+// const Guild = sequelize.define('guild', {
+// 	id: {
+// 		type: Sequelize.STRING,
+// 		unique: true,
+// 	}
+// });
+
+// const TextChannel = sequelize.define('textchannel', {
+// 	id: {
+// 		type: Sequelize.STRING,
+// 		unique: true,
+// 	}
+// });
+// 	Guild.hasMany(TextChannel);
+
+// const UserTextChannel = sequelize.define('user_channel', {
+// });
+// UserTextChannel.hasOne(User);
+// UserTextChannel.hasOne(TextChannel);
+
+// const UserTextChannelCommand = sequelize.define('user_textchannel_command', {
+// 	usages: {
+// 		type: Sequelize.INTEGER,
+// 		defaultValue: 0,
+// 	}
+// });
+// UserTextChannelCommand.hasOne(UserTextChannel);
+// UserTextChannelCommand.hasOne(Command);
+
+
 async function countCommand(command, user) {
 	let [userCommandUsageEntry,] = await UserCmdCount.findOrCreate({
 		where: {
 			userId: user.id,
-					commandName: command.name,
+			commandName: command.name,
 		},
 		defaults: {
 			amount: 0,
 		},
-				});
+	});
 	await userCommandUsageEntry.increment('amount');
 }
 
