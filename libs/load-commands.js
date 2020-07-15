8d9501037fdd00bc3c053f254078f9d0bbeb5502
Julien <jzapata@live.nl>
2020-07-15 23:39:39 +0200

    Add commands to database on load

--- libs/load-commands.js
+++ libs/load-commands.js
@@ -1,12 +1,26 @@
 const fs = require('fs');
+const { Command: CommandTable } = require('../ZapBot/sql');
 
-function load(dir, commands) {
+async function load(dir, commands) {
 	let commandFiles = fs.readdirSync(dir).filter(file => file.endsWith('.js'));
+	const CommandsInDB = await CommandTable.findAll({
+		attributes: ['name']
+	});
+	
 	for (let file of commandFiles) {
 		delete require.cache[require.resolve(`../${dir}/${file}`)];
 		let newCommand = require(`../${dir}/${file}`);
-		commands.set(newCommand.name, newCommand);
+		let commandName = newCommand.name;
+		if (notInDB(commandName, CommandsInDB)) {
+			await CommandTable.create({ name: commandName })
+			.then(() => console.log(`${commandName} was added to the Command table.`));
+		}
+		commands.set(commandName, newCommand);
 	}
+}
+
+function notInDB(commandName, CommandsInDB) {
+	return CommandsInDB.every(command => command.name != commandName);
 }
 
 module.exports.load = load;
